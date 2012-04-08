ba5325eb96ea503f3bce52e2be5a3b409b05a053
ronnysempai <rokgi45@hotmail.com>
2012-04-07 22:02:45 -0500

    tooltip

--- ToolTip/scriptToolTip.js
+++ ToolTip/scriptToolTip.js
@@ -1,14 +1,16 @@
 function simple_tooltip(target_items, name){
+
  $(target_items).each(function(i){
+ 
 		$("body").append("<div class='"+name+"' id='"+name+i+"'><p>"+$(this).attr('title')+"</p></div>");
 		var my_tooltip = $("#"+name+i);
-
+	
 		if($(this).attr("title") != ""){ // checks if there is a title
-
+			
 		$(this).removeAttr("title").mouseover(function(){
 				my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
 		}).mousemove(function(kmouse){
-				my_tooltip.css({left:kmouse.pageX+15, top:kmouse.pageY+15});
+				my_tooltip.css({left:kmouse.pageX+15, top:kmouse.pageY});
 		}).mouseout(function(){
 				my_tooltip.fadeOut(400);
 		});
@@ -19,4 +21,5 @@ function simple_tooltip(target_items, name){
 
 $(document).ready(function(){
 	 simple_tooltip("a","tooltip");
+	 
 });
