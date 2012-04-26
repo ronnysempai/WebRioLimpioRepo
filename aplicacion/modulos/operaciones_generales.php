c543b3d7e68da53a96d7f0823ae4cbb1919b479e
ronnysempai <rokgi45@hotmail.com>
2012-04-26 00:09:38 -0500

    primera ejecucion de primeras reglas en motor de inferencias

--- aplicacion/modulos/operaciones_generales.php
+++ aplicacion/modulos/operaciones_generales.php
@@ -5,109 +5,98 @@
 	require_once('../clases/ActividadAgricola.php');
 	require_once('../clases/Cultivo.php');
 	require_once('../clases/PracticaAgricola.php');
-	require_once('../clases/ControlReglas.php');
+	
 	
 	
 	
 	$datos=$_POST['datos'];
+	$arrFuentesContaminacion=Array();
 	
 	if(!empty($datos))
 	{
 		
 		$str_obj_json =$datos;
 			
-			echo '>>>'.$str_obj_json;
-			//$str_obj_json ='[ {"nombre": "valor"} , {"idCapa":"idcdf","nombre": "valor"} ]';
+			//echo '>>>'.$str_obj_json;
+			
 			
 			$obj_php = json_decode($str_obj_json);
 		
-			print_r($obj_php);
+			//print_r($obj_php);
 				for($i=0;  $i< count($obj_php); $i++)
 				{
-						echo '<p>*****'.$obj_php[$i]->idCapa.'</p>';
+					
+					
+						if(strrpos($obj_php[$i]->idCapa, "agricola"))
+						{	
+							
+							
+									$pendiente= new Pendiente();
+	
+									$pendiente->setPorcentaje($obj_php[$i]->pendiente);
+									
+									
+									$cultivo=new Cultivo();
+									$cultivo->setNombre($obj_php[$i]->cultivo);
+									$cultivo->setCiclo('largo');
+									
+									
+									$actividadAgricola=new ActividadAgricola();
+									
+									$actividadAgricola->setEstadoClima($obj_php[$i]->estadoClima);
+									
+									
+									
+
+									$actividadAgricola->pendiente=$pendiente;
+									$actividadAgricola->cultivo=$cultivo;
+									
+									
+									$practica1=new PracticaAgricola();
+									$actividadAgricola->practicaAgricola=$practica1;
+									
+									
+									$arrFuentesContaminacion[]=$actividadAgricola;
+							
+						}
+						else
+						if(strrpos($obj_php[$i]->idCapa, "ganadera"))
+						{
+							//to do
+						}
+						
 						
 				}
+				
+							foreach($arrFuentesContaminacion as $obj)
+							{
+										$wm = new WorkingMemory();
+										/*Agergacion de objeto a memoria de trabajo*/
+										$wm->insert($obj);
+										/*****/
+										$rr = new RuleReader();
+										/*leer archivo de reglas*/
+										$rs = new RuleSession($rr->parseFile("../base_de_conocimiento/reglas.srl"), $wm);
+										/******/
+										$rs->verbosity = 1;
+										/*correr reglas para este objeto*/
+										$rs->fireAll();
+										/**/
+							}
+				
+							/*mostrar los dtaos modificados por las reglas en cada objeto*/
+							foreach($arrFuentesContaminacion as $obj)
+							{
+								echo '<p></p>';
+								print_r($obj);
+								
+							}
+				
 				
 				exit;
 	}
 	
 	
-	$pendiente= new Pendiente();
-	
-	$pendiente->setPorcentaje(20);
-	
-	
-	$cultivo=new Cultivo();
-	$cultivo->setNombre('banano');
-	$cultivo->setCiclo('largo');
-	
-	
-	$actividadAgricola=new ActividadAgricola();
-	
-	$actividadAgricola->setEstadoClima('lluvioso');
-	
-	
-	
-
-	$actividadAgricola->pendiente=$pendiente;
-	$actividadAgricola->cultivo=$cultivo;
-	
-	
-	$practica1=new PracticaAgricola();
-	$actividadAgricola->practicaAgricola=$practica1;
-	
-	/////////////////////////////
-	
-	$pendiente2= new Pendiente();
-	
-	$pendiente2->setPorcentaje(35);
-	
-	
-	$cultivo2=new Cultivo();
-	$cultivo2->setNombre('algodon');
-	$cultivo2->setCiclo('corto');
-	
-	
-	
-	$actividadAgricola2=new ActividadAgricola();
-	
-	$actividadAgricola2->pendiente=$pendiente2;
-	
-	$actividadAgricola2->cultivo=$cultivo2;
-	$practica2=new PracticaAgricola();
-	$actividadAgricola2->practicaAgricola=$practica2;
-	
-	$arr[]=$actividadAgricola;
-	$arr[]=$actividadAgricola2;
-	//$objA=new A(1,3);
-	//$wm->insert($objA);
-	
-	foreach($arr as $obj)
-	{
-		$wm = new WorkingMemory();
-		
-	
-		$wm->insert($obj);
-	
-	
-	
-	$rr = new RuleReader();
-	//$rs = new RuleSession($rr->parseFile("test/rules/advanced_logic.srl"), $wm);
-	
-	
-	$rs = new RuleSession($rr->parseFile("../base_de_conocimiento/reglas.srl"), $wm);
-	
-	$rs->verbosity = 1;
-	$rs->fireAll();
-	
-	}
-	
-	
-	echo '<p>---</p>';
-	print_r($actividadAgricola);
-	echo '<p>---</p>';
-	print_r($actividadAgricola2);
-	echo '<p>---</p>';
 	
 				/*
 				$str_obj_json = '{
